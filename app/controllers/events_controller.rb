97e3cbd6af59148e691eebc26cb95ddb6331ea47
Luqman Musah <musahluqman@gmail.com>
2021-04-20 14:35:51 +0000

    Add event form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,18 @@
 class EventsController < ApplicationController
+    def new
+        @event = current_user.events.build 
+    end
+    def create
+        @event = current_user.events.build(params_events)
+        if @event.save
+        redirect_to events_url
+        else
+        render :new
+        end
+        end 
+private
+def params_events
+params.require(:events).permit(:title, :description) 
 end
+end
+
