ae5f858a6fadda6e8ec1ccef8fbbea21a34413c1
Addis Belete <addis.bel@gmail.com>
2021-04-22 09:27:25 +0300

    Modify Event show page to display attendees

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     @user = User.all
   end
 
-  def Show
+  def show
     @user = User.find(params[:id])
     @created_events = @user.events.all
   end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-  belongs_to :user, foreign_key: "user_id", class_name: "User"
+  belongs_to :user, class_name: "User"
   has_many :event_atendances, foreign_key: :attended_event_id
   has_many :attendees, through: :event_atendances
 end
